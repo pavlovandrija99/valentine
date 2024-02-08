6b68f6e3f515aaff02e6b8a460d1a207059776b3
Andrija Pavlov <pavlovandrija9@gmail.com>
2024-02-08 21:00:04 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const phrases = [
+  "No 🙉",
+  "Are you sure?",
+  "Really Sure😢",
+  "I'll be very sad😕",
+  "Pookie Please🥺",
+  "Don't do this to me🫤",
+  "I'm gonna cry..😭",
+  "You are breaking my heart😭💔",
+];
+
 function App() {
+  const [count, setCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = count * 5 + 16;
+
+  function handleYesBtnClick() {
+    setYesPressed(true);
+  }
+
+  function handleNoBtnClick() {
+    setCount(count + 1);
+  }
+
+  function getPhrase() {
+    return phrases[Math.min(count, phrases.length-1)];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="valentine-container">
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="Bear Kisses" />
+          <div className='win-text'>Yaaayyyy woohoo🙈🙈🙈 !!!!</div>
+        </>
+      ) : (
+        <>
+          <img className='kiss' src="https://media1.tenor.com/m/al4a1pG1f8YAAAAC/jump-bear.gif" alt="Jumping bear with heart"/>
+          <div className='text'>Will you be my valentine🌹?</div>
+          <div className='buttons'>
+            <button className='yes-button'
+                    style={{ fontSize: yesButtonSize, backgroundColor:'rgb(248, 229, 89)', color: 'black' }}
+                    onClick={handleYesBtnClick}
+            >
+              Yes 🙈
+            </button>
+            <button className='no-button'
+                    onClick={handleNoBtnClick}
+            >
+              {getPhrase()}
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
